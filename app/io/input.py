5a57292fd77d8236efaa923ab3c4584178cb26cc
Daria Karnaukh <dorakarnaukh@gmail.com>
2026-03-12 11:30:26 +0200

    Add input and output functions

---
+++ app/io/input.py
@@ -0,0 +1,13 @@
+def input_console():
+    """Read text from console"""
+    pass
+
+
+def read_file_builtin(path):
+    """Read text from file using built-in python"""
+    pass
+
+
+def read_file_pandas(path):
+    """Read text from file using pandas"""
+    pass
---
+++ app/io/output.py
@@ -0,0 +1,8 @@
+def print_console(text):
+    """Print text to console"""
+    pass
+
+
+def write_file_builtin(path, text):
+    """Write text to file"""
+    pass
